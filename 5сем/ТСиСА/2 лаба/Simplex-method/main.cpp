7d07eb63f4b59a0769aa85830409ce81c47c18f9
Alexander Kuleshov <saha262004@gmail.com>
2024-09-08 19:00:02 +0300

    Господи, оно работает (симплекс метод)

--- 5сем/ТСиСА/2 лаба/Simplex-method/main.cpp
+++ 5сем/ТСиСА/2 лаба/Simplex-method/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 template <typename T>
 std::ostream& operator<<(std::ostream &in, const std::vector<T> &vect) {
     int n = vect.size();
     for(int i =0;i<n;i++){
-        in << vect[i] << " " ;
+        in << fixed << setprecision(1) <<setw(4) << vect[i] << " " ;
     }
     return in;
 }
@@ -35,6 +36,22 @@ vector<T>& operator-=(vector<T>& vc, const vector<T>& sub) {
         vc[i] -= sub[i];
     }
     return vc;
+}
+
+template <typename T>
+vector<T>& operator/=(vector<T>& vc, T value) {
+    for(int i =0;i<vc.size();++i){
+        vc[i] /= value;
+    }
+    return vc;
+}
+
+void print(const vector< vector<long double> >& equations, const vector<long double>& F){
+    for(auto& vc : equations){
+        cout << vc << endl;
+    }
+    cout << F << endl;
+    cout << endl;
 }
 
 const long double eps = 0.001;
@@ -71,9 +88,55 @@ int get_row(int collumn_id, const vector< vector<long double> >& equations){
     return index;
 }
 
+int fix_negative_basis(vector< vector<long double> >& equations, vector<long double>& F){
+
+    cout << "fixing negative basis" << endl;
+    print(equations, F);
+
+    int row_id = -1;
+    for (int i = 0; i < equations.size(); ++i){
+        if (*equations[i].rbegin() < 0) row_id = i;
+    }
+    if (row_id == -1) return 0;
+
+    int col_id = -1;
+    long double min_ = 0;
+
+    for (int i = 0; i < equations[row_id].size() - 1; ++i){
+        if (equations[row_id][i] < min_ ){
+            min_ = equations[row_id][i];
+            col_id = i;
+        }
+    }
+
+    if (col_id == -1){
+        cout << "Solution does not exist" << endl;
+        exit(-2);
+    }
+
+    equations[row_id] /= equations[row_id][col_id];
+
+
+    for(int row = 0; row < equations.size(); ++row){
+        if (row == row_id) continue;
+
+
+        equations[row] -= equations[row_id] * equations[row][col_id] / equations[row_id][col_id];
+    }
+
+    F -= equations[row_id] * F[col_id] / equations[row_id][col_id];
+
+    return 1;
+}
+
 int iterate(vector< vector<long double> >& equations, vector<long double>& F){
+
+    while(fix_negative_basis(equations, F)){}
+
+    print(equations, F);
+
     int col_id = get_collumn(F);
-    cout << col_id << endl;
+
     if (col_id == -1){
         /// план оптимален, выходим
         cout << "Plan is optimal" << endl;
@@ -81,25 +144,37 @@ int iterate(vector< vector<long double> >& equations, vector<long double>& F){
     }
     int row_id = get_row(col_id, equations);
 
+    if (row_id == -1){
+        cout << "Plan is not bounded" << endl;
+        exit(-1);
+    }
+
+    cout << row_id << " " << col_id << endl;
+
     for(int row = 0; row < equations.size(); ++row){
         if (row == row_id) continue;
 
-        equations[row] -= equations[row_id] * equations[row][row_id] / equations[row_id][row_id];
+
+        equations[row] -= equations[row_id] * equations[row][col_id] / equations[row_id][col_id];
     }
 
-    F -= equations[row_id] * F[row_id] / equations[row_id][row_id];
+    F -= equations[row_id] * F[col_id] / equations[row_id][col_id];
     return 0;
 }
 
+
+
 void solve(vector< vector<long double> >& equations, vector<long double>& F){
 
+    print(equations, F);
+
     while(iterate(equations, F) == 0){
-        for(auto& vc : equations){
-            cout << vc << endl;
-        }
-        cout << F << endl;
+        print(equations, F);
     }
     cout << endl;
+
+    print(equations, F);
+
     for(int col = 0; col < equations[0].size(); ++col){
 
         int index = -1;
@@ -132,19 +207,63 @@ int main()
 
     */
 
-    vector<long double> F_input = {-3,-4}; /// F = 3*x1 + 4*x2 -> max
     vector< vector<long double> > equations_input;
+
+    vector<long double> F_input;
+
+
+    /** Пример из видео на ютубе
+    F_input = {-3,-4}; /// F = 3*x1 + 4*x2 -> max
 
     int n_input = 2; /// число переменных
     int m_input = 2; /// число уравнений
 
     equations_input.push_back({4,1,8}); /// 4x1 + x2 <= 8
     equations_input.push_back({-1,1,3}); /// -x1 + x2 <= 3
+    */
 
 
+    /**
+    /// Вариант 16, а
+    F_input = {-2,-5};
 
+    int m_input = 3; /// число уравнений
 
+    equations_input.push_back({1,2,8});
+    equations_input.push_back({1,1,6});
+    equations_input.push_back({-1,-3,-3});
+    */
 
+    /**
+    /// Вариант 16, б
+    F_input = {1,3};
+
+    int m_input = 3; /// число уравнений
+
+    equations_input.push_back({1,1,8});
+    equations_input.push_back({1,3,6});
+    equations_input.push_back({-1,-3,-3});
+    */
+
+    /**
+    /// Вариант 16, в
+    F_input = {-1,-3};
+
+    int m_input = 3; /// число уравнений
+
+    equations_input.push_back({-1,-2,-9});
+    equations_input.push_back({-1,-4,-8});
+    equations_input.push_back({-2,-1,-3});
+    */
+
+    /// Вариант 16, г
+    F_input = {-5,3};
+
+    int m_input = 3; /// число уравнений
+
+    equations_input.push_back({1,2,10});
+    equations_input.push_back({3,1,6});
+    equations_input.push_back({-1,-1,-16});
 
     vector<long double> F = F_input;
 
@@ -169,21 +288,10 @@ int main()
         }
         counter++;
         vc.push_back(last_elem);
-        cout << vc << endl;
     }
-    cout << F << endl;
-    iterate(equations, F);
 
-    cout << endl;
-    for(auto& vc : equations){
-        cout << vc << endl;
-    }
-    cout << F << endl;
-
-
-
-
-
+    cout << "input matrix" << endl;
+    print(equations, F);
     solve(equations, F);
 
     return 0;
